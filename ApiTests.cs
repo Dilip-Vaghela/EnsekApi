a95c09c0c4ce34149ecfb6b61bf15c797960c0fd
DillyV <dilip.vaghela@podium-solutions.co.uk>
2024-11-18 08:23:42 +0000

    Update +semver:minor

---
+++ ApiTests.cs
@@ -0,0 +1,79 @@
+using System.Net;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace EnsekApi;
+
+[TestFixture]
+public class Tests
+{
+    // The base URL for our example tests
+    private const string BASE_URL = "https://qacandidatetest.ensek.io/";
+
+    // The RestSharp client we'll use to make our requests
+    private RestClient client;
+
+    [OneTimeSetUp]
+    public void SetupRestSharpClient()
+    {
+        client = new RestClient(BASE_URL);
+    }
+
+    [Test]
+    public void TestResetTestData()
+    {
+        // setup login request
+        var requestLogin = new RestRequest("/ENSEK/login", Method.Post);
+        requestLogin.AddBody(new
+        {
+            password = "testing",
+            username = "test"
+        });
+        
+        // send request
+        var responseLogin = client.ExecutePost(requestLogin);
+        Assert.That(responseLogin.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        
+        // deserialize json string response to JsonNode object
+        var dataLogin = JsonSerializer.Deserialize<JsonNode>(responseLogin.Content!)!;
+        var bearerToken = dataLogin["access_token"];
+        
+        Assert.That(dataLogin["message"].ToString().Equals("Success"));
+        Assert.That(dataLogin["access_token"].AsValue().Equals(true));
+        Console.WriteLine($"""{dataLogin}""");
+        
+        // setup reset request with bearer token 
+        var requestReset = new RestRequest("/ENSEK/reset", Method.Post);
+        requestReset.AddOrUpdateHeader("Authorization", $"Bearer {bearerToken}");
+        
+        // send request
+        var responseReset = client.ExecutePost(requestReset);
+        Assert.That(responseLogin.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        var dataReset = JsonSerializer.Deserialize<JsonNode>(responseReset.Content!)!;
+        
+        Assert.That(dataReset["message"].ToString().Equals("Success"));
+        Console.WriteLine($"""{dataReset}""");
+    }
+    [TestCase(1,1)]
+    [TestCase(2,1)]
+    [TestCase(3,1)]
+    [TestCase(4,1)]
+    public void TestBuyAQuantityOfEachFuel(int id , int quantity)
+    {
+        var requestAddFuel = new RestRequest($"/ENSEK/buy/{id}/{quantity}", Method.Put);
+        var responseAddFuel = client.ExecutePut(requestAddFuel);
+        var dataAddFuel = JsonSerializer.Deserialize<JsonNode>(responseAddFuel.Content!)!;
+        Assert.That(dataAddFuel["message"].ToString().Contains("You have purchased "));
+        var orderId = dataAddFuel["message"].ToString().Substring(dataAddFuel["message"].ToString().Length - 37);
+        Console.WriteLine($"""{dataAddFuel}   asas  {orderId}""");
+        
+        var requestOrder = new RestRequest($"/ENSEK/orders", Method.Get);
+        var responseOrder = client.ExecuteGet(requestOrder);
+        var dataOrder = JsonSerializer.Deserialize<JsonNode>(responseOrder.Content!)!;
+        //Assert.That(dataOrder["Id"].ToString().Contains(orderId));
+        Console.WriteLine($"""{dataOrder}   asas  {orderId}""");
+    }
+    
+}
